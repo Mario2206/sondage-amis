35a58ab39ab003bf7531a51d264a095729340ed8
mathieu raimbault <mathieu.raimbault@hotmail.com>
2020-11-26 23:14:01 +0100

    :construction: Update model

--- Core/Model/Model.php
+++ Core/Model/Model.php
@@ -140,12 +140,12 @@ abstract class Model {
      * 
      * return int (number of updated rows)
      */
-    protected function _update (string $table, array $data, array $filters) : int {
+    protected function _update (array $data, array $filters) : int {
 
         $dataKeys = array_keys($data);
         $filterKey = array_keys($filters);
 
-        $query = QueryBuilder::update($table, $dataKeys);
+        $query = QueryBuilder::update($this->_tableName, $dataKeys);
 
         if($filters) {
             $query .= " " . QueryBuilder::filters($filterKey);
